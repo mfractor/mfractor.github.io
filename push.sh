0690639b306b47e4954e878c7af94bd3e8639371
Matthew Robbins <matthew.ch.robbins@gmail.com>
2017-06-08 11:37:39 +1000

    Add push script

---
+++ push.sh
@@ -0,0 +1,12 @@
+if [ $# -ne 1 ]
+  then
+    echo "Please supply a commit message so that the push script can run."
+    exit
+fi
+
+MESSAGE=$1
+
+git commit -am "$MESSAGE"
+git push
+
+mkdocs gh-deploy
