d0a65baec0bac1ae83e435f6dca802fd1532a7ec
Matthew Robbins <matthew@mfractor.com>
2019-11-23 14:28:34 +1000

    Deployed 31a0662 with MkDocs version: 1.0.4

---
+++ js/tabs.js
@@ -0,0 +1,8 @@
+function openTab(evt, tabName) {
+  var i;
+  var x = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
